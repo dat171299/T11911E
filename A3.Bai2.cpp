74de6b4ac160eb8a1c4e2d3ed6802939718c1555
dat171299 <50112937+dat171299@users.noreply.github.com>
2019-12-09 16:29:39 +0700

    Create A3.Bai2.cpp

---
+++ A3.Bai2.cpp
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+int main()
+{
+	int a;
+	
+	printf("Nhap a:");
+	scanf("%d",&a);
+	
+	if(a==2)
+	{
+		printf("thu 2");
+	}
+	else{
+	
+	    if(a==3)
+		{
+			printf("thu ba");
+		}
+		else{
+		
+		if(a==4)
+		{
+			printf("thu tu");
+		}
+	    else{
+	    	if(a==5)
+			{
+			printf("thu nam");
+			}
+			else{
+				if(a==6)
+				{
+					printf("thu sau");
+				}
+				else{
+					if(a==7)
+					{
+						printf("thu bay");
+					}
+					else{
+						if(a==8)
+						{
+							printf("chu nhat");
+						}
+						else{
+							printf("khong phai thu trong tuan");
+						}
+					}
+				}
+			}
+		}
+		}
+	
+    }
+	
+}
